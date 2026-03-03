4ad7052c52784216529cd788222049e3b9d95530
Smriti Goel <goelsmriti863@gmail.com>
2026-03-03 17:20:40 +0530

    Updated API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://carenest-backend-qo48.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
